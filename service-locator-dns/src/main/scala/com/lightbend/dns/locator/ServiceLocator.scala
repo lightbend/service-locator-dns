7908bf9ab0c99e70f9a7ff1a715fa2cf7067d615
Christopher Hunt <huntchr@gmail.com>
2016-09-29 19:51:40 +1000

    The initial DNS Service Locator
    An initial and tested version of the pure Akka based service locator for DNS.

---
+++ service-locator-dns/src/main/scala/com/lightbend/dns/locator/ServiceLocator.scala
@@ -0,0 +1,154 @@
+/*
+ * Copyright © 2016 Lightbend, Inc. All rights reserved.
+ * No information contained herein may be reproduced or transmitted in any form
+ * or by any means without the express written permission of Typesafe, Inc.
+ */
+
+package com.lightbend.dns.locator
+
+import akka.actor.{ Actor, ActorLogging, ActorRef, Props }
+import akka.io.AsyncDnsResolver.SrvResolved
+import akka.io.{ Dns, IO }
+import akka.pattern.{ AskTimeoutException, ask, pipe }
+import ru.smslv.akka.dns.raw.SRVRecord
+
+import scala.annotation.tailrec
+import scala.concurrent.Future
+import scala.util.Random
+import scala.util.matching.Regex
+
+object ServiceLocator {
+
+  def props: Props =
+    Props(new ServiceLocator)
+
+  /**
+   * Get one address given a service name. Names will be translated given a list of translator
+   * regexs provided as config. Only the first translation matched will be used. It is therefore
+   * important that the matchers are ordered carefully.
+   *
+   * An [[Addresses]] object will be replied with addresses sorted by priority and weight, as
+   * per RFC 2782. Only one of the addresses within the highest priority and randomized across weight
+   * will be returned.
+   */
+  case class GetAddress(name: String)
+
+  /**
+   * Get one or more addresses given a service name. Names are translated as per [[GetAddress]].
+   *
+   * An [[Addresses]] object will be replied with addresses sorted by priority and weight, as
+   * per RFC 2782.
+   */
+  case class GetAddresses(name: String)
+
+  /**
+   * A sequence of ServiceAddress objects are the reply, which may of course be empty.
+   */
+  case class Addresses(addresses: Seq[ServiceAddress])
+
+  /**
+   * Used within replies.
+   */
+  case class ServiceAddress(protocol: String, host: String, port: Int)
+
+  private[locator] case class RequestContext(replyTo: ActorRef, resolveOne: Boolean, srv: Seq[SRVRecord])
+
+  private[locator] case class ReplyContext(resolutions: Seq[(Dns.Resolved, SRVRecord)], rc: RequestContext)
+
+  @tailrec
+  private[locator] def matchName(name: String, nameTranslators: Seq[(Regex, String)]): Option[String] =
+    nameTranslators.headOption match {
+      case Some((r, s)) =>
+        val matcher = r.pattern.matcher(name)
+        if (matcher.matches())
+          Some(matcher.replaceAll(s))
+        else
+          matchName(name, nameTranslators.tail)
+      case None => None
+    }
+
+  private[locator] def protocolFromName(name: String): String =
+    name.dropWhile(_ != '.').drop(1).takeWhile(_ != '.').drop(1)
+}
+
+/**
+ * A service locator that can get all addresses for a service using DNS SRV lookups.
+ */
+class ServiceLocator extends Actor with ActorSettings with ActorLogging {
+
+  import ServiceLocator._
+
+  private val _dns = IO(Dns)(context.system)
+  protected def dns: ActorRef = _dns
+
+  private val random = new Random()
+
+  override def receive: Receive = {
+    case GetAddress(name) =>
+      resolve(name, resolveOne = true)
+
+    case GetAddresses(name) =>
+      resolve(name, resolveOne = false)
+
+    case rc: RequestContext =>
+      // TODO: Attend to priority and weight, although they appear pretty useless
+      // When we return just one address then we randomize which of the candidates to return
+      val srvFrom = if (rc.resolveOne && rc.srv.nonEmpty) random.nextInt(rc.srv.size) else 0
+      val srvSize = if (rc.resolveOne && rc.srv.nonEmpty) 1 else rc.srv.size
+      import context.dispatcher
+      val resolutions =
+        rc.srv
+          .slice(srvFrom, srvFrom + srvSize)
+          .map { srv =>
+            dns
+              .ask(Dns.Resolve(srv.target))(settings.resolveTimeout)
+              .mapTo[Dns.Resolved]
+              .map(_ -> srv)
+          }
+      Future
+        .sequence(resolutions)
+        .map(ReplyContext(_, rc))
+        .pipeTo(self)
+
+    case ReplyContext(resolutions, rc) =>
+      val addresses =
+        resolutions
+          .flatMap {
+            case (resolved, srv) =>
+              val protocol = protocolFromName(srv.name)
+              val port = srv.port
+              resolved.ipv4.map(host => ServiceAddress(protocol, host.getHostAddress, port)) ++
+                resolved.ipv6.map(host => ServiceAddress(protocol, host.getHostAddress, port))
+          }
+      rc.replyTo ! Addresses(addresses)
+
+    case iobe: IndexOutOfBoundsException =>
+      log.error("Could not substitute the service name with the name translator {}", iobe.getMessage)
+      sender() ! Addresses(List.empty)
+
+    case cce: ClassCastException =>
+      log.debug("Bad resolution - is this a properly formed SRV lookup of the form '_service._proto.some.domain'? {}", cce.getMessage)
+      sender() ! Addresses(List.empty)
+
+    case ate: AskTimeoutException =>
+      log.debug("Timed out on resolving an SRV record: {}", ate.getMessage)
+      sender() ! Addresses(List.empty)
+  }
+
+  private def resolve(name: String, resolveOne: Boolean): Unit = {
+    // TODO: Implement request level caching
+    log.debug("Resolving: {}", name)
+    val matchedName = matchName(name, settings.nameTranslators)
+    matchedName.foreach { mn =>
+      val replyTo = sender()
+      import context.dispatcher
+      dns
+        .ask(Dns.Resolve(mn))(settings.resolveTimeout)
+        .mapTo[SrvResolved]
+        .map(resolved => RequestContext(replyTo, resolveOne, resolved.srv))
+        .pipeTo(self)
+    }
+    if (matchedName.isEmpty)
+      sender() ! Addresses(List.empty)
+  }
+}
---
+++ service-locator-dns/src/main/scala/com/lightbend/dns/locator/Settings.scala
@@ -0,0 +1,47 @@
+/*
+ * Copyright © 2016 Lightbend, Inc. All rights reserved.
+ * No information contained herein may be reproduced or transmitted in any form
+ * or by any means without the express written permission of Typesafe, Inc.
+ */
+
+package com.lightbend.dns.locator
+
+import akka.actor.{ Actor, ExtendedActorSystem, Extension, ExtensionKey }
+
+import scala.concurrent.duration.{ Duration, FiniteDuration, MILLISECONDS }
+import com.typesafe.config.Config
+
+import scala.util.matching.Regex
+import scala.collection.JavaConversions._
+
+object Settings extends ExtensionKey[Settings]
+
+/**
+ * Settings for the service locator.
+ */
+class Settings(system: ExtendedActorSystem) extends Extension {
+
+  val nameTranslators: Seq[(Regex, String)] =
+    serviceLocatorDns
+      .getObjectList("name-translators")
+      .toList
+      .flatMap(_.toMap.map {
+        case (k, v) => k.r -> v.unwrapped().toString
+      })
+
+  val resolveTimeout: FiniteDuration =
+    duration(serviceLocatorDns, "resolve-timeout")
+
+  private lazy val config = system.settings.config
+  private lazy val serviceLocatorDns = config.getConfig("service-locator-dns")
+
+  private def duration(config: Config, key: String): FiniteDuration =
+    Duration(config.getDuration(key, MILLISECONDS), MILLISECONDS)
+}
+
+trait ActorSettings {
+  this: Actor =>
+
+  protected val settings: Settings =
+    Settings(context.system)
+}

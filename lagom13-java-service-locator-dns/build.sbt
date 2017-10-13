37999e8aa8d6d62fa57b467f1038527ff41353a9
Jason Longshore <longshorej@gmail.com>
2017-10-13 12:34:13 -0500

    Implement SBT 1.0 and Scala 2.12 cross-publishing support

--- lagom13-java-service-locator-dns/build.sbt
+++ lagom13-java-service-locator-dns/build.sbt
@@ -1,6 +1,7 @@
 name := "lagom13-java-service-locator-dns"
 
 libraryDependencies ++= Seq(
+  Library.akkaDns,
   Library.lagom13JavaClient,
   Library.akkaTestkit % "test",
   Library.scalaTest   % "test"
--- service-locator-dns/src/main/scala/com/lightbend/dns/locator/ServiceLocator.scala
+++ service-locator-dns/src/main/scala/com/lightbend/dns/locator/ServiceLocator.scala
@@ -1,7 +1,17 @@
 /*
- * Copyright © 2016 Lightbend, Inc. All rights reserved.
- * No information contained herein may be reproduced or transmitted in any form
- * or by any means without the express written permission of Typesafe, Inc.
+ * Copyright 2016 Lightbend, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.lightbend.dns.locator
@@ -182,9 +192,7 @@ class ServiceLocator extends Actor with ActorSettings with ActorLogging {
                 case _ =>
                   record
               }
-            }
-          )
-        )
+            }))
         .pipeTo(self)
     }
     if (matchedName.isEmpty)
@@ -197,7 +205,7 @@ class ServiceLocator extends Actor with ActorSettings with ActorLogging {
       .ask(Dns.Resolve(name))(resolveTimeout)
       .map {
         case srvResolved: SrvResolved => srvResolved
-        case _: Dns.Resolved          => SrvResolved(name, Nil)
+        case _: Dns.Resolved => SrvResolved(name, Nil)
       }
   }
 

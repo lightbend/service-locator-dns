dd78d8b6379c4496e662bb7f7a01c5091eb331be
Renato  Cavalcanti <renato@lightbend.com>
2018-01-23 15:42:29 +0100

    Lagom 1.4.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,8 +6,8 @@ object Version {
   // OSS
   val akka = "2.4.20"
   val akkaDns = "2.4.2"
-  val lagom13 = "1.3.7"
-  val lagom14 = "1.4.0-RC1"
+  val lagom13 = "1.3.10"
+  val lagom14 = "1.4.0"
   val scala212 = "2.12.3"
   val scala211 = "2.11.11"
   val scalaTest = "3.0.1"
